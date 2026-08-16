3bb07cacede8980177c0d5f3edc85564755276ca
salikazeemi1 <salikazeemi08@gmail.com>
2026-08-17 00:03:38 +0500

    Allow printing more than one error message

--- all_checks.py
+++ all_checks.py
@@ -2,6 +2,7 @@
 import shutil
 import sys
 import os
+
 def check_reboot():
     '''Returns True if the computer has a pending reboot'''
     return os.path.exists("/run/reboot-required")
@@ -23,10 +24,15 @@ def main():
     checks=[
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full")  
-	]
+    ]
+    everything_ok=True
     for check, msg in checks:
         if check():
-            sys.exit(1)
+            print(msg)
+            everything_ok= False
+
+    if not everything_ok:
+        sys.exit(1)
     
     print("Everything ok")
     sys.exit(0)
